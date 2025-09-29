a39d39ece584a79456a9467865b7d45d8a7689fb
abirmes <abirmeskini02@gmail.com>
2025-09-29 15:11:35 +0100

    using jupyter

--- data.py
+++ data.py
@@ -1,5 +1,8 @@
 # Importing the pandas library
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 df = pd.read_csv('assurance_maladie.csv')
 df.shape # Returns (number_of_rows, number_of_columns)
 df.info() #serves as a quick and effective tool during EDA to get a
@@ -9,12 +12,18 @@ df.describe() #statistical summary of numerical columns, including count, mean, 
 
 
 duplicates = df.duplicated() # Get duplicates
-
 df = df.drop_duplicates() #Remove Duplicates
 
 missing_values = df.isnull().sum() #Identifying Missing Values: The isnull() method, combined with sum(), helps identify missing values in each column.
 
-
-
-
 list = df.values.tolist()
+
+
+sns.scatterplot(data=df, x="bmi", y="charges") #Scatter Plot: To show the relationship between two numerical variables.
+plt.show()
+#Bar Plot: To display the average of a numerical variable across different categories.
+sns.barplot(data=df, x="sex", y="bmi")
+plt.show()
+#Distribution Plot (Displot): To visualize the distribution of a single numerical variable.
+sns.displot(data=df, x="bmi", kind="kde") # 'kind' can be 'hist', 'kde', 'ecdf'
+plt.show()
